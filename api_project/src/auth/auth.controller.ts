6848a11e8991428177ff3403e61800d5bec14221
maarij0160 <maarijamir1@gmail.com>
2023-07-26 15:22:05 +0500

    docker and postgre setup

--- api_project/src/auth/auth.controller.ts
+++ api_project/src/auth/auth.controller.ts
@@ -1,10 +1,29 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
 
 
-@Controller()
+@Controller('auth')
 export class AuthController{
 
     constructor(private authSerive:AuthService){}
+
+    @Post('signup')
+
+    signup(){
+        return 'I am signed up'
+    }
+
+    @Post('signin')
+
+    signin(){
+        return 'I am signed in'
+    }
+
+
+
+
+
+
 }
--- api_project/src/auth/auth.service.ts
+++ api_project/src/auth/auth.service.ts
@@ -1,6 +1,12 @@
 import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
 
 
 
 @Injectable({})
-export class AuthService{}
+export class AuthService{
+
+    constructor(private prisma:PrismaService){
+
+    }
+}
